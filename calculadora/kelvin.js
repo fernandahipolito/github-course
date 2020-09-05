0c1d1056d31ba20f1274cad8af366de5b0e63b05
Fernanda Hipolito <fernanda_hipolito@hotmail.com>
2020-09-05 16:24:27 -0300

    adicionando newton

--- calculadora/kelvin.js
+++ calculadora/kelvin.js
@@ -2,7 +2,11 @@ let kelvin = 294;  //atribui 294 à variável kelvin
 let celsius = kelvin - 273;  //variável celsius
 let fahrenhein = celsius * (9/5) + 32; //variável fehrenheit
 Math.floor (fahrenheit);
-console.log ('A temperatura é de : ' + fahrenheit + 'graus fahrenheit.'
+console.log ('A temperatura é de : ' + fahrenheit + 'graus fahrenheit.';
+let newton = celsius * (33/100);
+Math.floor (newton);
+console.log ('A temperatura é de ' + newton + 'Newtons');
+
 
 
 
