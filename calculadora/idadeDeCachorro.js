9c8d57184809ccd3f22e0645fc13a34f3e969aa4
Fernanda Hipolito <fernanda_hipolito@hotmail.com>
2020-09-05 17:24:16 -0300

    entrega do desafio 01

---
+++ calculadora/idadeDeCachorro.js
@@ -0,0 +1,18 @@
+let minhaIdade = 42; //variável minha idade
+let primeirosAnos = 2; //equivalência dos primeiros anos
+primeirosAnos  *=10.5;
+let anosSeguintes = minhaIdade - 2;// cálculo da idade nos anos seguintes
+anosSeguintes *=4; //cálculo da idade dos cachorros nos anos seguintes
+let minhaIdadeDeCachorro = primeirosAnos + anosSeguintes; //minha idade se fosse um cachorro;
+'Fernanda'.toLowerCase(); //fernanda
+let meuNome = 'Fernanda';// atribuição à variável meu nome
+console.log ('Meu nome é ' + meuNome + '. ' + 'Eu tenho ' + minhaIdade + 'anos em idade de gente que equivalem a ' + minhaIdadeDeCachorro + 'anos em idade de cachorros. '); //retorna a minha idade se fosse cachoorro
+
+
+
+
+
+
+
+
+
