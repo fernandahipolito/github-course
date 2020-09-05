34fd00cdc806272723de44f2958ade4509fd6a16
Fernanda Hipolito <fernanda_hipolito@hotmail.com>
2020-09-05 16:10:51 -0300

    Adicionando arquivo kelvin.js

---
+++ calculadora/kelvin.js
@@ -0,0 +1,8 @@
+let kelvin = 294;  //atribui 294 à variável kelvin
+let celsius = kelvin - 273;  //variável celsius
+let fahrenhein = celsius * (9/5) + 32; //variável fehrenheit
+Math.floor (fahrenheit);
+console.log ('A temperatura é de : ' + fahrenheit + 'graus fahrenheit.'
+
+
+
